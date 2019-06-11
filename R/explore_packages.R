039b64705805e92ebe07fa92e7ccf1a0ffaa0c16
Markus Skyttner <markus.skyttner@nrm.se>
2019-06-11 10:02:54 +0200

    add script to list installed packages, and data

---
+++ R/explore_packages.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(here)
+library(readr)
+
+.libPaths()
+
+ipt <- 
+  installed.packages() %>%
+  as_tibble() %>%
+  select(Package, LibPath, Version, Priority, Built)
+
+dir.create(here("data"))
+
+ipt %>% write_csv(path = here("data", "installed-packages.csv"))
